ec3d68de53a04b488cb7476eb1f8b39504920f28
Stefan Auditor <stefan.auditor@erdfisch.de>
2010-03-15 16:55:31 +0000

    Updating, coding standards

---
+++ js/check_status.bbb.js
@@ -0,0 +1,15 @@
+//$Id$
+Drupal.behaviors.bbbCheckStatusInit = function () {
+  Drupal.bbbCheckStatus();
+  setInterval("Drupal.bbbCheckStatus();", 5000);
+}
+
+Drupal.bbbCheckStatus = function () {
+  var url = bbb_check_status_url;
+  $.getJSON(url, function(data) {
+    console.log(data);
+    if (data.is_running == true) {
+      location.href = location.href + '/meeting/attend';
+    }
+  });
+}

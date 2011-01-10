ee572c3cd573bb7d41a4420d1c577f2500e0ef35
Stefan Auditor <stefan.auditor@erdfisch.de>
2011-01-10 15:38:38 +0000

    #1016910 by kristofkeppens, sanduhrs: Changed Drupal 7 port of bbb module.

--- js/check_status.bbb.js
+++ js/check_status.bbb.js
@@ -1,15 +1,18 @@
 //$Id$
-Drupal.behaviors.bbbCheckStatusInit = function () {
-  Drupal.bbbCheckStatus();
-  setInterval("Drupal.bbbCheckStatus();", 5000);
-}
-
-Drupal.bbbCheckStatus = function () {
-  var url = bbb_check_status_url;
-  $.getJSON(url, function(data) {
-    console.log(data);
-    if (data.running == true) {
-      location.href = location.href + '/meeting/attend';
-    }
-  });
-}
+(function($) {
+    Drupal.behaviors.bbbCheckStatus = {
+        attach : function(context, settings) {
+            Drupal.bbbCheckStatus();
+            setInterval("Drupal.bbbCheckStatus();", 5000);
+        }
+    };
+    Drupal.bbbCheckStatus = function() {
+        var url = bbb_check_status_url;
+        $.getJSON(url, function(data) {
+            console.log(data);
+            if (data.running == true) {
+                location.href = location.href + '/meeting/attend';
+            }
+        });
+    };
+})(jQuery);
